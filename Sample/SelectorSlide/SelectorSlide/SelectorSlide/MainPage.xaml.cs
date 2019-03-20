e75b3aa750d5b55f055d2c1b8d90bf9e204c9901
Noe miranda <noe_is10@msn.com>
2019-03-20 16:02:08 -0700

    sample added

---
+++ Sample/SelectorSlide/SelectorSlide/SelectorSlide/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SelectorSlide
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            SelectorSlideSse.SetOptions(new List<string>
+            {
+                "MX$20",
+                "MX$30",
+                "MX$40",
+                "MX$50+"
+            });
+
+            /*SelectorSlideSse.SetBackgroundColor(Color.Violet);
+            SelectorSlideSse.OptionHideColor = Color.Green;
+            SelectorSlideSse.OptionShowColor = Color.Red;
+            SelectorSlideSse.SetThumbColor(Color.Brown);
+            SelectorSlideSse.SetMinimumTrackColor(Color.Blue);
+            SelectorSlideSse.SetMaximumTrackColor(Color.DarkMagenta);
+            ^*/
+
+            SelectorSlideSse.OnPositionChanged += (sse, pos)=>{
+                Debug.WriteLine("new pos = " + pos);
+                Debug.WriteLine("current pos = " + sse.GetPosition());
+            };
+
+        }
+
+    }
+}
